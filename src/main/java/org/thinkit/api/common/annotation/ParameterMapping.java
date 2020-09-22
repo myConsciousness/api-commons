4ade68879e5b1bcc44bc87b8d1ab253b9f9e8e8e
Kato Shinya <yourdream28@gmail.com>
2020-09-22 10:51:42 +0900

    add

---
+++ src/main/java/org/thinkit/api/common/annotation/ParameterMapping.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.api.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * HTTP通信のリクエスト送信時に付加するリクエストパラメータのマッピングを行うアノテーションです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ParameterMapping {
+
+    /**
+     * HTTO通信時に使用するリクエストパラメータのキー名を管理します。リクエストパラメータの値を設定するフィールドへこのアノテーションを付加し、
+     * 実際にHTTP通信をする際に使用するパラメータ名を渡してください。
+     *
+     * @return HTTP通信時に使用するパラメータのキー名
+     */
+    String paramaterKey();
+}
