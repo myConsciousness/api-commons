00bc0283ef201b59dce70f956bc747bdd9dc284b
Kato Shinya <yourdream28@gmail.com>
2020-09-24 10:49:39 +0900

    update

--- src/main/java/org/thinkit/api/common/ResourcePath.java
+++ src/main/java/org/thinkit/api/common/ResourcePath.java
@@ -14,6 +14,8 @@
 
 package org.thinkit.api.common;
 
+import java.util.Arrays;
+
 import lombok.NonNull;
 
 /**
@@ -33,14 +35,37 @@ public interface ResourcePath {
      */
     public String getResourcePath();
 
+    /**
+     * リソースパスに引数として渡された値をバインドして返却します。
+     * <p>
+     * 値をバインドさせるためにはあらかじめリソースパス中にバインド変数を定義する必要があります。リソースパスへの値のバインド処理は先頭から順番に
+     * {@link String#format(String, Object...)} メソッドを使用して行われます。
+     * <p>
+     * 例えば、リソースパス中に3つのバインド変数を定義した場合は以下のように使用してください。
+     *
+     * <pre>
+     * <code>
+     * String resourcePath = ConcreteResourcePath.bind(var1, var2, var3);
+     * </code>
+     * </pre>
+     *
+     * @param parameters リソースパスへバインドする値。可変長引数なためカンマ区切りでバインドする値を指定してください。
+     * @return 引数として渡された値がバインドされたリソースパス
+     *
+     * @exception NullPointerException     引数として渡された値が {@code null} の場合
+     * @exception IllegalArgumentException バインドする対象の値が引数として渡されなかった場合
+     */
     default String bind(@NonNull String... parameters) {
 
-        // TODO Preconditionに置き換える
         if (parameters.length <= 0) {
             throw new IllegalArgumentException();
         }
 
         String resourcePath = this.getResourcePath();
+
+        for (String parameter : Arrays.asList(parameters)) {
+            resourcePath = String.format(resourcePath, parameter);
+        }
 
         return resourcePath;
     }
